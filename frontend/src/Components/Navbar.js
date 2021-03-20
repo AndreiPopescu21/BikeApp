c2e2064a582bf5059c08f20f23d2609484bfa08d
Alex Constantin <alex.constantin432@gmail.com>
2021-03-20 22:07:10 +0000

    fIX top speed

--- frontend/src/Components/Navbar.js
+++ frontend/src/Components/Navbar.js
@@ -5,15 +5,14 @@ import { Link } from 'react-router-dom'
 const NavbarComponent = () => {
     return (
     <Navbar bg="dark" expand="lg" variant="dark">
-    <Navbar.Brand as = {Link} to = "/">Bike App</Navbar.Brand>
+    <Navbar.Brand as = {Link} to = "/">spreadBike</Navbar.Brand>
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto container-fluid">
         <Nav.Link as = {Link} to = "/">Home</Nav.Link>
         <Nav.Link as = {Link} to = "/tracker">Tracker</Nav.Link>
-        <Nav.Link as = {Link} to = "/gps">GPS</Nav.Link>
+        <Nav.Link as = {Link} to = "/gps">Route</Nav.Link>
         <Nav.Link as = {Link} to = "/rent">Rent Bikes</Nav.Link>
-        <Nav.Link as = {Link} to = "/">Leaderboards</Nav.Link>
         <NavDropdown title="Other" id="basic-nav-dropdown">
             <NavDropdown.Item as = {Link} to = "/fitness">Fitness</NavDropdown.Item>
             <NavDropdown.Item as = {Link} to = "/">Average Details</NavDropdown.Item>
